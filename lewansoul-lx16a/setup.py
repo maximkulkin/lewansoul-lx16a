70467ca1a4a7e89197684a44513497c24116622f
Maxim Kulkin <maxim.kulkin@gmail.com>
2019-09-07 00:50:48 -0700

    Add PyPI classifiers

--- lewansoul-lx16a/setup.py
+++ lewansoul-lx16a/setup.py
@@ -9,5 +9,17 @@ setup(
     url='https://github.com/maximkulkin/lewansoul-lx16a',
     py_modules=['lewansoul_lx16a'],
     license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
     install_requires=['pyserial'],
 )
